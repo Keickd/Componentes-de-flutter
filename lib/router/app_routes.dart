83a20fcb64a797d0295c7c777c8b91bed31818e8
Keickd <keickd16@gmail.com>
2022-08-14 11:34:58 +0200

    establecimiento mas organizado de rutas

---
+++ lib/router/app_routes.dart
@@ -0,0 +1,58 @@
+import 'package:fl_components/models/menu_option.dart';
+import 'package:fl_components/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static const initialRoute = 'home';
+
+  static final menuOptions = <MenuOption>[
+    //TODO BORRAR HOME
+    MenuOption(
+        route: 'home',
+        name: 'Home Screeen',
+        screen: const HomeScreen(),
+        icon: Icons.home),
+    MenuOption(
+        route: 'listview1',
+        name: 'ListView tipo 1',
+        screen: const ListView1Screen(),
+        icon: Icons.list),
+    MenuOption(
+        route: 'listview2',
+        name: 'ListView tipo 2',
+        screen: const ListView2Screen(),
+        icon: Icons.list_alt),
+    MenuOption(
+        route: 'alert',
+        name: 'Alerts',
+        screen: const AlertScreen(),
+        icon: Icons.add_alert),
+    MenuOption(
+        route: 'card',
+        name: 'Cards',
+        screen: const CardScreen(),
+        icon: Icons.credit_card)
+  ];
+
+  static Map<String, Widget Function(BuildContext)> getAppRoutes() {
+    Map<String, Widget Function(BuildContext)> appRoutes = {};
+
+    for (final option in menuOptions) {
+      appRoutes.addAll({option.route: (BuildContext context) => option.screen});
+    }
+
+    return appRoutes;
+  }
+
+  // static Map<String, Widget Function(BuildContext)> routes = {
+  //   'home': (BuildContext context) => const HomeScreen(),
+  //   'listview1': (BuildContext context) => const ListView1Screen(),
+  //   'listview2': (BuildContext context) => const ListView2Screen(),
+  //   'alert': (BuildContext context) => const AlertScreen(),
+  //   'card': (BuildContext context) => const CardScreen()
+  // };
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    return MaterialPageRoute(builder: (context) => const AlertScreen());
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/router/app_routes.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,12 +14,20 @@ class HomeScreen extends StatelessWidget {
       ),
       body: ListView.separated(
           itemBuilder: (context, index) => ListTile(
-                leading: const Icon(Icons.ac_unit_rounded),
-                title: const Text('Nombre de ruta'),
-                onTap: () {},
+                leading: Icon(AppRoutes.menuOptions[index].icon,
+                    color: Colors.indigo),
+                title: Text(AppRoutes.menuOptions[index].name),
+                onTap: () {
+                  // final route = MaterialPageRoute(
+                  //     builder: (context) => const ListView1Screen());
+
+                  // Navigator.pushReplacement(context, route);
+                  Navigator.pushNamed(
+                      context, AppRoutes.menuOptions[index].route);
+                },
               ),
           separatorBuilder: (_, __) => const Divider(),
-          itemCount: 10),
+          itemCount: AppRoutes.menuOptions.length),
     );
   }
 }
