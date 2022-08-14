e68f189d5d42bb824ffa5e18fd22af2df1a0594f
Keickd <keickd16@gmail.com>
2022-08-14 14:36:40 +0200

    mejoras en el card widget y text button

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:fl_components/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -6,10 +6,13 @@ class CardScreen extends StatelessWidget {
 
   @override
   Widget build(Object context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Card Widget'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+          children: const [CustomCardType1()],
+        ));
   }
 }
